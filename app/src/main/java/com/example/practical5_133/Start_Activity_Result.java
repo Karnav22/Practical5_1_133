7625919f6ddd2bcce2ef527763ca757342a673ae
Administrator <karnavshah2002@gmail.com>
2023-09-13 12:56:48 +0530

    practical 5 133

--- app/src/main/java/com/example/practical5_133/Start_Activity_Result.java
+++ app/src/main/java/com/example/practical5_133/Start_Activity_Result.java
@@ -2,7 +2,11 @@ package com.example.practical5_133;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Start_Activity_Result extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class Start_Activity_Result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_result);
+        Intent intent = getIntent();
+        final int number1 = intent.getIntExtra("number1", 0);
+        final int number2 = intent.getIntExtra("number2", 0);
+
+        TextView textViewNumbers = findViewById(R.id.textView);
+        textViewNumbers.setText("Numbers: " + number1 + ", " + number2);
+
+        Button buttonAdd = findViewById(R.id.Add);
+        Button buttonSubtract = findViewById(R.id.Sub);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = number1 + number2;
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("result", result);
+
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+        buttonSubtract.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int result = number1 - number2;
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("result", result);
+
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
     }
 }
